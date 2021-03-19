a20a4b86aa18fae5f429542354e7b0c263591c37
nicoszpako <nicoszpako@aol.com>
2021-03-19 16:59:12 +0100

    Updated doc to v1.0.4-a

--- src/main/java/fr/nico/sqript/structures/IOperation.java
+++
@@ -1,8 +0,0 @@
-package fr.nico.sqript.structures;
-
-import fr.nico.sqript.types.ScriptType;
-
-@FunctionalInterface
-public interface IOperation {
-    ScriptType<?> operate(ScriptType<?> o1, ScriptType<?> o2);
-}
--- src/main/java/fr/nico/sqript/types/interfaces/ILocatable.java
+++
@@ -1,9 +0,0 @@
-package fr.nico.sqript.types.interfaces;
-
-import net.minecraft.util.math.BlockPos;
-
-public interface ILocatable {
-
-    public BlockPos getPos();
-
-}
