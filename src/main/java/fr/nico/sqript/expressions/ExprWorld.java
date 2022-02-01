92eb36c276f902caaf63e179de13db3a4071dc61
Kerlann <kerlann.pro@gmail.com>
2022-02-01 22:48:24 +0100

    Add ExprWorld.java

--- src/main/java/fr/nico/sqript/expressions/ExprWorld.java
+++ src/main/java/fr/nico/sqript/expressions/ExprWorld.java
@@ -4,6 +4,7 @@ import fr.nico.sqript.meta.Expression;
 import fr.nico.sqript.meta.Feature;
 import fr.nico.sqript.structures.ScriptContext;
 import fr.nico.sqript.types.*;
+import fr.nico.sqript.types.primitive.TypeNumber;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 @Expression(name = "Entities Expressions",
         features = {
                 @Feature(name = "World", description = "Returns world.", examples = "world", pattern = "world", type = "world"),
+                @Feature(name = "World time", description = "Returns the world time.", examples = "world time", pattern = "{world} time", type = "number"),
                 @Feature(name = "World Entities BoundingBox", description = "Return list of entity entities in the boudingbox.", examples = "world", pattern = "{string} with in {axisalignedbb}", type = "array"),
         }
 )
@@ -23,7 +25,10 @@ public class ExprWorld extends ScriptExpression {
                 return new TypeWorld(FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld());
             case 1:
                 World world = FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld();
+                return new TypeNumber(world.getWorldTime());
+            case 2:
                 AxisAlignedBB axisAlignedBB = (AxisAlignedBB) parameters[1].getObject();
+                world = FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld();
                 String entityClassName = (String) parameters[0].getObject();
                 try {
                     Class <? extends Entity > aClass = (Class< ? extends Entity >) Class.forName(entityClassName);
