1d0d34bdf271f810bdbf0dcb2141ba42807b6d8e
nicoszpako <nicoszpako@aol.com>
2022-02-21 21:56:55 +0100

    "keys of {dictionary}" expression

--- src/main/java/fr/nico/sqript/expressions/ExprDictionaries.java
+++ src/main/java/fr/nico/sqript/expressions/ExprDictionaries.java
@@ -8,10 +8,13 @@ import fr.nico.sqript.types.ScriptType;
 import fr.nico.sqript.types.TypeArray;
 import fr.nico.sqript.types.TypeDictionary;
 
+import java.util.Collection;
+
 @Expression(name = "Dictionaries",
         features = {
                 @Feature(name = "Dictionary", description = "Returns a new dictionary.", examples = "set {my_dictionary} to a new dictionary", pattern = "[a] [new] dictionary", type = "dictionary"),
                 @Feature(name = "Dictionary from an array of key-value couples", description = "Returns a new dictionary based on elements of an array that represent key-value couples.", examples = "set {my_dictionary} to a new dictionary with [[\"Key 1\",\"Value 1\"]]", pattern = "[a] [new] dictionary with {array*}", type = "dictionary"),
+                @Feature(name = "Keys of dictionary", description = "Returns an array containing the keys of the given dictionary.", examples = "keys of dictionary with [[\"Key 1\",\"Value 1\"]] #[\"Key 1\"]", pattern = "keys of {dictionary}", type = "array"),
         }
 )
 public class ExprDictionaries extends ScriptExpression {
@@ -28,6 +31,13 @@ public class ExprDictionaries extends ScriptExpression {
                     d.getObject().put(pair.getObject().get(0), pair.getObject().get(1));
                 }
                 return d;
+            case 2:
+                d = ((TypeDictionary) parameters[0]);
+                TypeArray result = new TypeArray();
+                for (ScriptType scriptType : d.getObject().keySet()) {
+                    result.getObject().add(scriptType);
+                }
+                return result;
         }
         return null;
     }
